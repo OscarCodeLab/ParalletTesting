5c52ddeae78c5b8c3339005390b0757bd2e8ba83
OscarCodeLab <kolawoleopeyemioscar@gmail.com>
2024-12-31 23:09:10 +0100

    chnage froma action to jsExecutor class

--- SauceLabAutomationWithParralletTesting/src/main/java/com/SauceLab/page/CartPage.java
+++ SauceLabAutomationWithParralletTesting/src/main/java/com/SauceLab/page/CartPage.java
@@ -2,6 +2,7 @@ package com.SauceLab.page;
 
 import java.time.Duration;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -18,11 +19,12 @@ import com.SauceLab.base.BasePage;
 public class CartPage extends BasePage {
 	private Actions actions;
 	private WebDriverWait wait;
-
+	private JavascriptExecutor jsExecutor;
 	public CartPage(WebDriver driver) {
 		super(driver);
 		actions = new Actions(driver);
 		this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+	    this.jsExecutor = (JavascriptExecutor) driver;
 		logger.info("CartPage initialized with WebDriver: " + driver);
 	}
 
@@ -42,17 +44,25 @@ public class CartPage extends BasePage {
 	}
 
 	public InformationPage clickCheckoutButton() {
-		logger.info("Attempting to click on the Checkout button.");
-		try {
-			wait.until(ExpectedConditions.elementToBeClickable(checkoutButton));
-			actions.moveToElement(checkoutButton).click().build().perform();
-			logger.info("Checkout button clicked successfully.");
-		} catch (Exception e) {
-			logger.error("Error occurred while clicking the Checkout button: {}", e.getMessage());
-			throw e;
-		}
-		return new InformationPage(driver);
+	    logger.info("Attempting to click on the Checkout button.");
+	    try {
+	        // Wait until the element is clickable
+	        wait.until(ExpectedConditions.elementToBeClickable(checkoutButton));
+
+	        // Scroll the element into view
+	        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", checkoutButton);
+
+	        // Perform the click using JavaScript for precision
+	        jsExecutor.executeScript("arguments[0].click();", checkoutButton);
+
+	        logger.info("Checkout button clicked successfully.");
+	    } catch (Exception e) {
+	        logger.error("Error occurred while clicking the Checkout button: {}", e.getMessage());
+	        throw new RuntimeException("Failed to click on the Checkout button.", e);
+	    }
+	    return new InformationPage(driver);
 	}
+
 
 	public InventoryPage clickContinueShoppingButton() {
 		logger.info("Attempting to click on the Continue Shopping button.");
