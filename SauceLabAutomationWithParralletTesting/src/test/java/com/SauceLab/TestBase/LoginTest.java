fc74a5810156f8304a9c87a0b49dae5eaed50172
OscarCodeLab <kolawoleopeyemioscar@gmail.com>
2025-01-01 15:22:50 +0100

    changing in script

---
+++ SauceLabAutomationWithParralletTesting/src/test/java/com/SauceLab/TestBase/LoginTest.java
@@ -0,0 +1,31 @@
+package com.SauceLab.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.SauceLab.BaseClass.Base;
+import com.SauceLab.Utilities.DataProviders;
+import com.SauceLab.page.InventoryPage;
+import com.SauceLab.page.LoginPage;
+
+public class LoginTest extends Base{
+ private LoginPage login;
+ private InventoryPage inventory;
+	
+	@Test(priority = 1)
+    public void verifyImage() {
+        login = new LoginPage(getDriver());
+        boolean isLogoDisplayed = login.verifyLogo();
+        Assert.assertTrue(isLogoDisplayed, "Logo is not displayed!");
+    }
+	
+	 @Test(priority = 2, dataProvider = "dp", dataProviderClass = DataProviders.class)
+	    public void sauceLoginTest(String username, String password) {
+	        login = new LoginPage(getDriver());
+	        inventory = new InventoryPage(getDriver());
+
+	        inventory = login.clickLogin(username, password);
+	        String actual = inventory.getText();
+	        String expected = "Products";
+	        Assert.assertEquals(actual, expected);
+	    }
+}
